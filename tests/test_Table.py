f89fc788bfe7d1587936f5c170a20754f127a32a
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-11-03 00:10:48 -0200

    Improved tests readability using the dedent function (the problem is that it breaks code folding and aesthetics)

--- tests/test_Table.py
+++ tests/test_Table.py
@@ -15,6 +15,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from textwrap import dedent
+
 import unittest
 import tempfile
 import os
@@ -27,72 +29,72 @@ from outputty import Table
 class TestTable(unittest.TestCase):
     def test_table_with_only_one_header_without_data(self):
         my_table = Table(headers=['test'])
-        self.assertEqual(str(my_table), '''
-+------+
-| test |
-+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +------+
+        | test |
+        +------+
+        ''').strip())
         my_table = Table(headers=['blabla'])
-        self.assertEqual(str(my_table), '''
-+--------+
-| blabla |
-+--------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +--------+
+        | blabla |
+        +--------+
+        ''').strip())
 
     def test_table_with_many_headers_without_data(self):
         my_table = Table(headers=['spam', 'ham'])
-        self.assertEqual(str(my_table), '''
-+------+-----+
-| spam | ham |
-+------+-----+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +------+-----+
+        | spam | ham |
+        +------+-----+
+        ''').strip())
         my_table = Table(headers=['ham', 'spam', 'eggs'])
-        self.assertEqual(str(my_table), '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        ''').strip())
 
     def test_table_with_many_headers_and_one_row_same_size(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
         my_table.rows.append({'ham': 123, 'spam': 4567, 'eggs': 8910})
-        self.assertEqual(str(my_table), '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-| 123 | 4567 | 8910 |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        | 123 | 4567 | 8910 |
+        +-----+------+------+
+        ''').strip())
 
     def test_table_with_many_headers_and_rows_same_size(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
         my_table.rows.append({'ham': 123, 'spam': 4567, 'eggs': 8910})
         my_table.rows.append({'ham': 321, 'spam': 7654, 'eggs': 1098})
         my_table.rows.append({'ham': 'abc', 'spam': 'defg', 'eggs': 'hijk'})
-        self.assertEqual(str(my_table), '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-| 123 | 4567 | 8910 |
-| 321 | 7654 | 1098 |
-| abc | defg | hijk |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        | 123 | 4567 | 8910 |
+        | 321 | 7654 | 1098 |
+        | abc | defg | hijk |
+        +-----+------+------+
+        ''').strip())
 
     def test_table_with_many_headers_and_rows_missing_some_columns(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
         my_table.rows.append({'spam': 4567, 'eggs': 8910})
         my_table.rows.append({'ham': 321, 'eggs': 1098})
         my_table.rows.append({'ham': 'abc', 'spam': 'defg'})
-        self.assertEqual(str(my_table), '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-|     | 4567 | 8910 |
-| 321 |      | 1098 |
-| abc | defg |      |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        |     | 4567 | 8910 |
+        | 321 |      | 1098 |
+        | abc | defg |      |
+        +-----+------+------+
+        ''').strip())
 
     def test_table_with_changed_separators(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'], dash='=', pipe='-',
@@ -100,13 +102,14 @@ class TestTable(unittest.TestCase):
         my_table.rows.append({'ham': '', 'spam': '', 'eggs': ''})
         my_table.rows.append({'ham': 1, 'spam': 2, 'eggs': 3})
         my_table.rows.append({'ham': 11, 'spam': 22, 'eggs': 33})
-        self.assertEqual(str(my_table), ''' ===== ====== ======
-- ham - spam - eggs -
- ===== ====== ======
--     -      -      -
--   1 -    2 -    3 -
--  11 -   22 -   33 -
- ===== ====== ====== ''')
+        self.assertEqual(str(my_table), dedent('''\
+         ===== ====== ====== 
+        - ham - spam - eggs -
+         ===== ====== ====== 
+        -     -      -      -
+        -   1 -    2 -    3 -
+        -  11 -   22 -   33 -
+         ===== ====== ====== '''))
 
     def test_table_should_accept_rows_as_dict_list_tuple_int_or_float(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
@@ -114,51 +117,51 @@ class TestTable(unittest.TestCase):
         my_table.rows.append([1, 42, 3])
         my_table.rows.append([3.14, 2.71, 0.0])
         my_table.rows.append(('spam', 'eggs', 'ham'))
-        self.assertEqual(str(my_table), '''
-+------+------+------+
-| ham  | spam | eggs |
-+------+------+------+
-| eggs |  ham | spam |
-|    1 |   42 |    3 |
-| 3.14 | 2.71 |  0.0 |
-| spam | eggs |  ham |
-+------+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +------+------+------+
+        | ham  | spam | eggs |
+        +------+------+------+
+        | eggs |  ham | spam |
+        |    1 |   42 |    3 |
+        | 3.14 | 2.71 |  0.0 |
+        | spam | eggs |  ham |
+        +------+------+------+
+        ''').strip())
 
     def test_table_should_accept_headers_as_dict_list_tuple_int_or_float(self):
         my_table = Table(headers=[42, 3.14, (4, 2), [3, 14], {'answer': 42}])
-        self.assertEqual(str(my_table), '''
-+----+------+--------+---------+----------------+
-| 42 | 3.14 | (4, 2) | [3, 14] | {'answer': 42} |
-+----+------+--------+---------+----------------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +----+------+--------+---------+----------------+
+        | 42 | 3.14 | (4, 2) | [3, 14] | {'answer': 42} |
+        +----+------+--------+---------+----------------+
+        ''').strip())
 
     def test_table_with_many_headers_and_rows_right_aligned(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
         my_table.rows.append({'ham': '', 'spam': '', 'eggs': ''})
         my_table.rows.append({'ham': 1, 'spam': 2, 'eggs': 3})
         my_table.rows.append({'ham': 11, 'spam': 22, 'eggs': 33})
-        self.assertEqual(str(my_table), '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-|     |      |      |
-|   1 |    2 |    3 |
-|  11 |   22 |   33 |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        |     |      |      |
+        |   1 |    2 |    3 |
+        |  11 |   22 |   33 |
+        +-----+------+------+
+        ''').strip())
 
     def test_table_with_headers_little_than_rows(self):
         my_table = Table(headers=['ham', 'spam', 'eggs'])
         my_table.rows.append({'ham': 'ham spam ham', 'spam': 'spam eggs spam',
                               'eggs': 'eggs ham eggs'})
-        self.assertEqual(str(my_table), '''
-+--------------+----------------+---------------+
-|     ham      |      spam      |      eggs     |
-+--------------+----------------+---------------+
-| ham spam ham | spam eggs spam | eggs ham eggs |
-+--------------+----------------+---------------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +--------------+----------------+---------------+
+        |     ham      |      spam      |      eggs     |
+        +--------------+----------------+---------------+
+        | ham spam ham | spam eggs spam | eggs ham eggs |
+        +--------------+----------------+---------------+
+        ''').strip())
 
     def test_output_to_csv_should_create_the_file_correctly_with_headers(self):
         temp_fp = tempfile.NamedTemporaryFile()
@@ -174,28 +177,30 @@ class TestTable(unittest.TestCase):
         fp.close()
         os.remove(temp_fp.name)
 
-        self.assertEquals(contents, '''"ham","spam","eggs"
-"ham spam ham","spam eggs spam","eggs ham eggs"
-''')
+        self.assertEquals(contents, dedent('''\
+        "ham","spam","eggs"
+        "ham spam ham","spam eggs spam","eggs ham eggs"
+        '''))
 
     def test_should_import_data_from_csv(self):
         temp_fp = tempfile.NamedTemporaryFile(delete=False)
-        temp_fp.write('''"ham","spam","eggs"
-"ham spam ham","spam eggs spam","eggs ham eggs"
-"ham spam","eggs spam","eggs eggs"
-''')
+        temp_fp.write(dedent('''\
+        "ham","spam","eggs"
+        "ham spam ham","spam eggs spam","eggs ham eggs"
+        "ham spam","eggs spam","eggs eggs"
+        '''))
         temp_fp.close()
 
         my_table = Table(from_csv=temp_fp.name)
         os.remove(temp_fp.name)
-        self.assertEquals(str(my_table), '''
-+--------------+----------------+---------------+
-|     ham      |      spam      |      eggs     |
-+--------------+----------------+---------------+
-| ham spam ham | spam eggs spam | eggs ham eggs |
-|     ham spam |      eggs spam |     eggs eggs |
-+--------------+----------------+---------------+
-'''.strip())
+        self.assertEquals(str(my_table), dedent('''
+        +--------------+----------------+---------------+
+        |     ham      |      spam      |      eggs     |
+        +--------------+----------------+---------------+
+        | ham spam ham | spam eggs spam | eggs ham eggs |
+        |     ham spam |      eggs spam |     eggs eggs |
+        +--------------+----------------+---------------+
+        ''').strip())
 
     def test_should_save_data_into_text_file(self):
         temp_fp = tempfile.NamedTemporaryFile(delete=False)
@@ -211,52 +216,52 @@ class TestTable(unittest.TestCase):
         contents = fp.read()
         fp.close()
         os.remove(temp_fp.name)
-        self.assertEqual(contents, '''
-+-----+------+------+
-| ham | spam | eggs |
-+-----+------+------+
-|     |      |      |
-|   1 |    2 |    3 |
-|  11 |   22 |   33 |
-+-----+------+------+
-        '''.strip())
+        self.assertEqual(contents, dedent('''
+        +-----+------+------+
+        | ham | spam | eggs |
+        +-----+------+------+
+        |     |      |      |
+        |   1 |    2 |    3 |
+        |  11 |   22 |   33 |
+        +-----+------+------+
+        ''').strip())
 
     def test_character_count_in_row_data_should_use_unicode(self):
         my_table = Table(headers=['First name', 'Last name'])
         my_table.rows.append({'First name': 'Álvaro', 'Last name': 'Justen'})
         my_table.rows.append(['Tatiana', 'Al-Chueyr'])
         my_table.rows.append(('Flávio', 'Amieiro'))
-        self.assertEqual(str(my_table), '''
-+------------+-----------+
-| First name | Last name |
-+------------+-----------+
-|     Álvaro |    Justen |
-|    Tatiana | Al-Chueyr |
-|     Flávio |   Amieiro |
-+------------+-----------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +------------+-----------+
+        | First name | Last name |
+        +------------+-----------+
+        |     Álvaro |    Justen |
+        |    Tatiana | Al-Chueyr |
+        |     Flávio |   Amieiro |
+        +------------+-----------+
+        ''').strip())
 
     def test_character_count_in_headers_should_be_unicode(self):
         my_table = Table(headers=['ÁÀÃÂÇ', 'ÇÉÈẼÊ'])
         my_table.rows.append(('spam', 'eggs'))
         my_table.rows.append(('eggs', 'spam'))
-        self.assertEqual(str(my_table), '''
-+-------+-------+
-| ÁÀÃÂÇ | ÇÉÈẼÊ |
-+-------+-------+
-|  spam |  eggs |
-|  eggs |  spam |
-+-------+-------+
-        '''.strip())
+        self.assertEqual(str(my_table), dedent('''
+        +-------+-------+
+        | ÁÀÃÂÇ | ÇÉÈẼÊ |
+        +-------+-------+
+        |  spam |  eggs |
+        |  eggs |  spam |
+        +-------+-------+
+        ''').strip())
 
     def test_input_and_ouput_character_encoding_in_headers(self):
         my_table = Table(headers=['Álvaro'.decode('utf8').encode('utf16')],
                          input_encoding='utf16', output_encoding='iso-8859-1')
-        self.assertEqual(str(my_table), '''
-+--------+
-| Álvaro |
-+--------+
-        '''.strip().decode('utf8').encode('iso-8859-1'))
+        self.assertEqual(str(my_table), dedent('''
+        +--------+
+        | Álvaro |
+        +--------+
+        ''').strip().decode('utf8').encode('iso-8859-1'))
 
     def test_input_and_output_character_encoding_in_method_to_csv(self):
         temp_fp = tempfile.NamedTemporaryFile(delete=False)
@@ -285,13 +290,13 @@ class TestTable(unittest.TestCase):
         file_contents = fp.read()
         fp.close()
         os.remove(temp_fp.name)
-        output = '''
-+--------+
-| Álvaro |
-+--------+
-|  Píton |
-+--------+
-        '''.strip().decode('utf8').encode('iso-8859-1')
+        output = dedent('''
+        +--------+
+        | Álvaro |
+        +--------+
+        |  Píton |
+        +--------+
+        ''').strip().decode('utf8').encode('iso-8859-1')
         self.assertEqual(file_contents, output)
 
     def test_output_character_encoding_in_method___str__(self):
@@ -299,13 +304,13 @@ class TestTable(unittest.TestCase):
                          input_encoding='utf16', output_encoding='iso-8859-1')
         my_table.rows.append(['Píton'.decode('utf8').encode('utf16')])
 
-        output = '''
-+--------+
-| Álvaro |
-+--------+
-|  Píton |
-+--------+
-        '''.strip().decode('utf8').encode('iso-8859-1')
+        output = dedent('''
+        +--------+
+        | Álvaro |
+        +--------+
+        |  Píton |
+        +--------+
+        ''').strip().decode('utf8').encode('iso-8859-1')
         self.assertEqual(str(my_table), output)
 
     def test_input_and_output_character_encoding_in_parameter_from_csv(self):
@@ -316,24 +321,24 @@ class TestTable(unittest.TestCase):
         my_table = Table(from_csv=temp_fp.name, input_encoding='iso-8859-1',
                          output_encoding='utf16')
         os.remove(temp_fp.name)
-        output = '''
-+--------+
-| Álvaro |
-+--------+
-|  Píton |
-+--------+
-        '''.strip().decode('utf8').encode('utf16')
+        output = dedent('''
+        +--------+
+        | Álvaro |
+        +--------+
+        |  Píton |
+        +--------+
+        ''').strip().decode('utf8').encode('utf16')
         self.assertEqual(str(my_table), output)
 
     def test___unicode__should_return_unicode_no_matter_the_input_encoding(self):
         my_table = Table(headers=['ÁÀÃÂÇ', 'ÇÉÈẼÊ'])
         my_table.rows.append(('spam', 'eggs'))
         my_table.rows.append(('eggs', 'spam'))
-        self.assertEqual(unicode(my_table), '''
-+-------+-------+
-| ÁÀÃÂÇ | ÇÉÈẼÊ |
-+-------+-------+
-|  spam |  eggs |
-|  eggs |  spam |
-+-------+-------+
-        '''.strip().decode('utf8'))
+        self.assertEqual(unicode(my_table), dedent('''
+        +-------+-------+
+        | ÁÀÃÂÇ | ÇÉÈẼÊ |
+        +-------+-------+
+        |  spam |  eggs |
+        |  eggs |  spam |
+        +-------+-------+
+        ''').strip().decode('utf8'))
